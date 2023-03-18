f876db4e4cb98798b0f7fb21236227a406260641
LazarZsolt13 <75862569+LazarZsolt13@users.noreply.github.com>
2023-03-18 22:16:19 +0200

    Login/Register

--- movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/authentication/RegisterFragment.kt
+++ movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/authentication/RegisterFragment.kt
@@ -7,10 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import edu.hackathon.moviematch.R
 import edu.hackathon.moviematch.databinding.FragmentLoginBinding
+import edu.hackathon.moviematch.databinding.FragmentRegisterBinding
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentRegisterBinding? = null
     val binding get() = _binding!!
 
 
@@ -19,15 +20,13 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
 
     }
-
-
 }
