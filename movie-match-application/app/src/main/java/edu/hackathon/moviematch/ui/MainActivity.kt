2c28ef68ab27cffb977c55c2e23bb45598de6470
LazarZsolt13 <75862569+LazarZsolt13@users.noreply.github.com>
2023-03-19 00:48:00 +0200

    Bottom_Nav

--- movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/MainActivity.kt
+++ movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+
 import edu.hackathon.moviematch.R
 import edu.hackathon.moviematch.databinding.ActivityMainBinding
 
@@ -12,8 +16,12 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -21,6 +29,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.fragment_welcome_search)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //navController = binding.navHostFragmentContainer.findNavController()
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.fragment_feed -> {
+                    navController.navigateUp()
+                    navController.navigate(R.id.fragment_feed)
+                }
+                R.id.fragment_mood -> {
+                    navController.navigateUp()
+                    navController.navigate(R.id.fragment_mood)
+                }
+//                R.id.thirdBotNav -> {
+//                    navController.navigateUp()
+//                    navController.navigate(R.id.profileDetailFragment)
+//                }
+            }
+            true
+        }
+
     }
 
     override fun onDestroy() {
