f72b870c8fd6f1b59d8d88bb86fee63900da8c6c
BHunor <burszanhunor@gmail.com>
2023-03-18 21:34:35 +0200

    Login Fragment

---
+++ movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/authentication/LoginFragment.kt
@@ -0,0 +1,30 @@
+package edu.hackathon.moviematch.ui.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.hackathon.moviematch.R
+import edu.hackathon.moviematch.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+
+    }
+
+
+}
