553ac8a30ed13b690f7cd692e3f1f987d7de5223
simoncsaby <79094535+simoncsaby@users.noreply.github.com>
2023-03-19 09:49:57 +0200

    rate

--- movie-match-application/app/src/main/java/edu/hackathon/moviematch/api/Constants.kt
+++ movie-match-application/app/src/main/java/edu/hackathon/moviematch/api/Constants.kt
@@ -1,8 +1,8 @@
 package edu.hackathon.moviematch.api
 object Constants {
-    const val API_BASE_URL = "";
+    const val API_BASE_URL = "192.168.43.35:8082/";
     const val OPEN_AI_API_URL = "https://api.openai.com/";
-    const val OPEN_AI_TOKEN = "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"
+    const val OPEN_AI_TOKEN = "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"
     const val MOVIE_DB_API_URL = "https://api.themoviedb.org/"
     const val MOVIE_DB_API_KEY = "55b7f8db758832ecfab9176b81841cdf"
 }
--- movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/welcome/GridAdapter.kt
+++ movie-match-application/app/src/main/java/edu/hackathon/moviematch/ui/welcome/GridAdapter.kt
@@ -4,12 +4,11 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.media.Image
 import android.transition.Transition
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.AbsListView
-import android.widget.BaseAdapter
-import android.widget.ImageView
+import android.widget.*
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -22,14 +21,12 @@ interface IOnItemClickListener {
 }
 class GridAdapter(private val context: Context, private val searchFilmResultResponses: MutableList<SearchFilmResultResponse>, private val listener:IOnItemClickListener) : BaseAdapter() {
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                val imageView: ImageView
-                if (convertView == null) {
-                        imageView = ImageView(context)
-                        imageView.layoutParams = AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 600)
-                        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-                } else {
-                        imageView = convertView as ImageView
-                }
+                val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.grid_item, parent, false)
+                var imageView = view.findViewById<ImageView>(R.id.grid_image)
+                val textView = view.findViewById<TextView>(R.id.grid_text)
+                textView.text = "Rate: "+"%.1f".format(searchFilmResultResponses.get(position).rating) + "/10"
+                imageView.layoutParams = RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 600)
+                imageView.scaleType = ImageView.ScaleType.CENTER_CROP
                 Glide.with(context)
                         .load("https://image.tmdb.org/t/p/w500/"+searchFilmResultResponses.get(position).posterPath)
                         .placeholder(R.drawable.rounded)
@@ -41,7 +38,7 @@ class GridAdapter(private val context: Context, private val searchFilmResultResp
                         imageView.startAnimation(anim)
                         listener.onItemClick(searchFilmResultResponses.get(position))
                 }
-                return imageView
+                return view
         }
 
         override fun getItem(position: Int): Any {
